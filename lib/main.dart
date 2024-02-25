392b46eca34a98e3155f52c32c25020936cf24d3
mcorona02 <mcoronaptv@gmail.com>
2024-02-25 01:14:53 -0500

    updated to be shared

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,45 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('something something odyssey T^T',
-          style : TextStyle(
-            fontSize: 50,
-          )),
+      home : Scaffold(
+        body : Center(
+          child : Stack(
+            alignment : Alignment.center,
+            children : [
+              Positioned(
+                top : 150,
+                child : Text(
+                  'title idk T^T',
+                  style : TextStyle(
+                    fontSize: 150,
+                  ),
+                ),
+              ),
+              Positioned(
+                top : 375, // Adjust the top position as needed
+                child : ElevatedButton(
+                  onPressed : null, // Set to null if no action is required
+                  child : Text(
+                    'Start',
+                    style : TextStyle(
+                      fontSize: 50,
+                    )),
+                ),
+              ),
+              Positioned(
+                top : 475,
+                child : ElevatedButton(
+                  onPressed : null,
+                  child : Text(
+                    'Options',
+                    style : TextStyle(
+                      fontSize : 50, 
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
